ae5c8623ba712114ce837a35139534f3545c0417
roli123 <53379137+roli123@users.noreply.github.com>
2021-10-19 13:12:58 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -6,3 +6,5 @@ tar -zcvf "/tmp/roli-httpd-logs-$(date).tar.gz" /var/log/apache2/
 aws s3 \
 cp /tmp/roli-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar \
 s3://roliautomationproject/roli-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
+cat /var/www/html/inventory.html
+cat /etc/cron.d/automation
