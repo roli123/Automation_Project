2ca535315d399a724a629695d82c76fd51480707
roli123 <53379137+roli123@users.noreply.github.com>
2021-10-19 12:15:25 +0530

    Add files via upload

---
+++ automation.sh
@@ -0,0 +1,8 @@
+sudo apt update -y
+apt install apache2
+/etc/init.d/apache2 start
+systemctl enable apache2
+tar -zcvf "/tmp/roli-httpd-logs-$(date).tar.gz" /var/log/apache2/
+aws s3 \
+cp /tmp/roli-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar \
+s3://roliautomationproject/roli-httpd-logs-$(date '+%d%m%Y-%H%M%S').tar
